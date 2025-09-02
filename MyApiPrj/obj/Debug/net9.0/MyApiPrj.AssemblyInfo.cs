2624ea14f3e75a3a067595e300ddda019b778a94
soheil-moonesi <s.moonesi@alumni.znu.ac.ir>
2025-09-02 15:32:40 +0430

    Add initial setup note

--- MyApiPrj/obj/Debug/net9.0/MyApiPrj.AssemblyInfo.cs
+++ MyApiPrj/obj/Debug/net9.0/MyApiPrj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiPrj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff01dfbaf4021bcad43af82a01e8c9527e96596")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d56902d40962a8610edbccddda0c0e03662b322")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiPrj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiPrj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
