42b6c9ca3e7b2c5c50ebc002e80f61609afb3094
soheil <soheil.moonesi@gmail.com>
2025-09-04 18:50:54 -0400

    add All Fonts in AsciiArt

--- MyApiPrj/AsciiArt.cs
+++ MyApiPrj/AsciiArt.cs
@@ -24,8 +24,24 @@ public static class AsciiArt
         //Uses standard if no property is found
         font ??= FiggleFonts.Standard;
         //Returns as string instead of writing to console
-        return font.Render(text);  
+        return font.Render(text);
     }
+    //Lazy static property
+    //Lazy means that the property’s value isn’t established until its first use.
+    public static Lazy<IEnumerable<string>> AllFonts = new(
+        //Anonymous method to initialize lazy property
+        () =>
+        //Uses LINQ to query the data
+        from p in typeof(FiggleFonts).
+        //GetProperties returns an array of PropertyInfo objects, one for each FiggleFont property
+        GetProperties(BindingFlags.Static | BindingFlags.Public)
+        //Projects the name of the font
+              select p.Name);
+              
+//     Lazy-initialized properties are useful when it’s expensive to build something and/or
+    // the values created take up memory and you want to pay that cost only if those values
+    // are used. A list of font names isn’t a substantial use of memory, and reflection is rela-
+    // tively fast, so it’s not necessary to use Lazy in this situation, but it doesn’t hurt.
 
 
 }
