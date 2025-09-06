728f83a9c465d702e15ae628c0a8642230a9783b
soheil-moonesi <s.moonesi@alumni.znu.ac.ir>
2025-09-06 17:44:36 +0430

    Try to Add Database Connection

--- MyApiPrj/AddApplicationServices.cs
+++ MyApiPrj/AddApplicationServices.cs
@@ -15,17 +15,29 @@
 using System.Reflection;
 using Application.Common.Behaviour;
 using MediatR;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
 
 namespace Application
 {
     public static class ConfigureServices
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        //! why , when i add service AddScope<IUnitOfWork,UnitOfWork>
+        //!  automatically add object to method under?
+        public static object AddApplicationServices(this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestResponseMediateRDemo<,>));
-
-
+            //reason to not AddScope<IRepository,Repository> is :
+            //The Core Problem: Losing the Shared Context
+            // The most important reason for favoring the Unit of Work registration is transactional integrity and shared context management.
+            // Multiple Repositories, One Context: The UnitOfWork class in your example is designed to create and manage all repositories for a single HTTP request. 
+            //It passes the same DbContext instance to every repository it creates.
+            // Single Save Point: When you call _unitOfWork.SaveChanges(), it calls SaveChanges() on that one shared DbContext.
+            //  This ensures that all changes made by any repository during the request are saved together as a single transaction. Either all succeed, or all fail.      
+            services.AddScoped<IUnitOfWork, UnitofWork>();
+            //services.AddDbContext<MustCreateDbContext>(option=>option.UseSqlite(Configuration.GetConnectionString("")))
             return services;
         }
 //Use TRequest/TResponse when working directly with MediatR interfaces
