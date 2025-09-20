9b0a23e0c3907324be826442d3073fddd490e9a8
soheil-moonesi <s.moonesi@alumni.znu.ac.ir>
2025-09-20 17:54:57 +0430

    clean architecture Part 2

--- MyApiPrj/obj/Debug/net9.0/MyApiPrj.AssemblyInfo.cs
+++ MyApiPrj/obj/Debug/net9.0/MyApiPrj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApiPrj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4d360141e4f87a3d77db0517214b1e580d4040")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73e9a6dd9d9f375b31017ed5e947f3804a7fcb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApiPrj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApiPrj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/infrastructure/RawPrj.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RawPrj.Data
+{
+    public class Class1
+    {
+    }
+}
