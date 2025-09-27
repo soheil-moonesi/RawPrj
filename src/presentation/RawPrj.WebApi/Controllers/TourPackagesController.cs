91b955a82993e68c12c43d254d7e2c1d999b29bb
soheil-moonesi <s.moonesi@alumni.znu.ac.ir>
2025-09-27 12:51:50 +0330

    Tour Package CRUD done

--- src/presentation/RawPrj.WebApi/Controllers/TourPackagesController.cs
+++ src/presentation/RawPrj.WebApi/Controllers/TourPackagesController.cs
@@ -20,12 +20,50 @@ namespace RawPrj.WebApi.Controllers
         }
 
 
+        //api/tourpackages Get Request
         [HttpGet]
         public IActionResult Get()
         {
             return Ok(_context.tourPackages);
 
         }
+
+        //api/tourpackages Post Request
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TourPackage tourPackage)
+        {
+            await _context.tourPackages.AddAsync(tourPackage);
+            await _context.SaveChangesAsync();
+            return Ok(tourPackage);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var tourPackage = await _context.tourPackages.SingleOrDefaultAsync(tp => tp.Id == id);
+
+            if (tourPackage == null)
+            {
+                return NotFound();
+            }
+            _context.tourPackages.Remove(tourPackage);
+            await _context.SaveChangesAsync();
+            return Ok(tourPackage);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] TourPackage tourPackage)
+        {
+
+            _context.Update(tourPackage);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(tourPackage);
+
+        }
+
 
 
 
